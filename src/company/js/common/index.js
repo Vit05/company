b0057eb31f68ed76124d30dc1a7d6f433ff1e583
Vit05 <viktorpaliy88@gmail.com>
2019-04-26 18:04:12 +0300

    fix bugs

--- src/company/js/common/index.js
+++ src/company/js/common/index.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //  MOBILE NAVIGATION
     var mobNavBtn = document.getElementById('mobNavBtn');
+    var body = document.body;
 
     mobNavBtn.addEventListener('click', function () {
         var toggleMobileNav = document.querySelector('.main_navigation');
 
+
         toggleMobileNav.classList.toggle("in")
+        body.classList.toggle("no_scroll")
         mobNavBtn.childNodes[1].classList.toggle('active')
     })
 });
@@ -15,7 +18,7 @@ var swiper = new Swiper('.leader_slider .swiper-container', {
     slidesPerView:1,
     loop:true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.leader_slider_next',
+        prevEl: '.leader_slider_prev',
     },
 });
